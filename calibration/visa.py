e5ea4ddd8a0d44e1209c68e10c7da99530286380
alexfy1015 <tidus1015@gmail.com>
2025-11-02 22:33:15 -0500

    prototyped download data function to get stock data from yahoo finance.

--- calibration/visa.py
+++ calibration/visa.py
@@ -1,9 +1,21 @@
 import yfinance as yf
+import pandas as pd
 
-# Download S&P 500 index data (ticker: ^GSPC)
-sp500 = yf.download('^GSPC', start='2000-01-01', end='2025-01-01')
+def download_data(ticker, start_date, end_date, data_dir='data', data_name='data'):
+    raw = yf.download(ticker, start=start_date, end=end_date)
+    raw.to_csv(f'{data_dir}/raw_data_{data_name}.csv')
+    df = raw.reset_index().iloc[:,[0, 1]]
+    df.columns = ['date', 'close_price']
+    df.to_csv(f'{data_dir}/close_price_data_{data_name}.csv', index=False)
+    return df
 
-# Save to CSV
-sp500.to_csv('sp500_data.csv')
+df_sp500 = download_data('^GSPC', '2000-01-01', '2025-01-01', data_name='sp500')
+df_visa = download_data('V', '2000-01-01', '2025-01-01', data_name='visa')
 
-print("S&P 500 data downloaded and saved to sp500_data.csv")
+# merge
+
+# compute log return
+
+# perform regression analysis
+
+# statistical testing
